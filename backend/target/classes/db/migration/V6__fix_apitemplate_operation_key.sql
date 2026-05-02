e28e9d97c417cd83c253d443ede5eab3e90cd8c2
darecare <darko_z@yahoo.com>
2026-05-02 17:26:19 +0000

    fiscal bill module

--- backend/target/classes/db/migration/V6__fix_apitemplate_operation_key.sql
+++
@@ -1,4 +0,0 @@
--- Fix operationKey for MerchantPro orders fetch template to use standardised code format
-UPDATE apitemplate
-SET operation_key = 'FETCH_ORDERS'
-WHERE operation_key = 'Get Orders';
